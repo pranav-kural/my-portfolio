32ec78272e21381cd3e5d09b021b74f5190fffc5
Pranav Kural <pranavkural042@gmail.com>
2017-04-30 17:21:38 -0400

    Created the website structure and Views

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
+  <!-- Parent Vue -->
   <div id="app">
+    <!-- website components -->
     <site-header></site-header>
     <about-me></about-me>
+    <my-work></my-work>
+    <my-skills></my-skills>
+    <contact-me></contact-me>
+    <site-footer></site-footer>
   </div>
 </template>
 
 <script>
+// Importing the website compenents
 import header from './vues/Header.vue';
 import aboutMe from './vues/AboutMe.vue';
+import myWork from './vues/MyWork.vue';
+import mySkills from './vues/MySkills.vue';
+import contactMe from './vues/ContactMe.vue';
+import footer from './vues/Footer.vue';
 
+// Exporting the Top most parent component - App
 export default {
   name: 'app',
   components: {
     'site-header': header,
-    'about-me': aboutMe
+    'about-me': aboutMe,
+    'my-work': myWork,
+    'my-skills': mySkills,
+    'contact-me': contactMe,
+    'site-footer': footer
   }
 }
 </script>
 
 <style>
+/* Importing the website style sheet */
 @import "assets/css/styles.css";
 </style>
---
+++ src/vues/MySkills.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: "mySkills"
+}
+</script>
+
+<style lang="css">
+</style>
