ea1b96d6215b354c34fb075f6d93eff5ac1477e2
Pranav Kural <pranavkural042@gmail.com>
2017-05-08 02:45:37 -0400

    Added the C# skills

--- src/vues/charts/CSharpSkills.vue
+++ src/vues/charts/CSharpSkills.vue
@@ -1,5 +1,18 @@
 <template lang="html">
-
+  <div class="uk-flex uk-flex-center uk-padding-small">
+    <div class="uk-container-small">
+      <p><strong>VBScript</strong> was the first dynamic language that I learnt in my High School. Then moving on to <strong>ASP</strong> and then finally developing desktop application using <strong>C#</strong> and <strong>.NET framework</strong>. Following some of my skills in this area:</p>
+      <ul class="uk-list uk-list-divider skillPoints uk-margin-left uk-padding-small">
+        <li><strong>Rapid Application Development</strong> using .NET and Windows Forms</li>
+        <li>Knowledge and understanding of <abbr title="Object Oriented Programming"><strong>OOP</strong></abbr> concepts like Inheritance, Encapsulation, Polymorphism and Abstraction</li>
+        <li>Working with various <strong>Data Structures</strong> avaiable in C# (Lists, Dictionary)</li>
+        <li><strong>File I/O</strong> and <strong>Exception handling</strong></li>
+        <li>Using C# for <strong>Unity Game Development</strong></li>
+        <li>Working with <strong>Entity Framework</strong> for data intensive applications</li>
+        <li><strong>ASP.NET</strong></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/vues/charts/JavaSkills.vue
+++ src/vues/charts/JavaSkills.vue
@@ -1,17 +1,17 @@
 <template lang="html">
   <div class="uk-flex uk-flex-center uk-padding-small">
     <div class="uk-container-small">
-      <p>Java was the first Object Oriented language I started learning. I've took three courses during my study in the Diploma programme which were primarly based on Java. Following are the skills I've gained working with Java programming lanugage:</p>
+      <p><strong>Java</strong> was the first <strong>Object Oriented</strong> and <strong>statically typed language</strong> I learnt. I've took three courses during my study in the Diploma programme which were primarly based on Java. Following are the skills I've gained working with Java programming lanugage:</p>
       <ul class="uk-list uk-list-divider skillPoints uk-margin-left uk-padding-small">
-        <li>Knowledge and understanding of Inheritance, Encapsulation, Polymorphism, and Abstraction</li>
-        <li>Working with various collections or Data Structures avaiable in Java (List, Map, Set)</li>
-        <li>File I/O and Serialization</li>
-        <li>Exception handling (checked and unchecked)</li>
-        <li>Working with the JVM and Memory management</li>
-        <li>Basic understanding of Multithreading and synchronization</li>
-        <li>Java GUI development with Swing/AWT and Java FX.</li>
+        <li>Knowledge and understanding of <abbr title="Object Oriented Programming"><strong>OOP</strong></abbr> concepts like Inheritance, Encapsulation, Polymorphism, and Abstraction</li>
+        <li>Working with various collections or <strong>Data Structures</strong> avaiable in Java (List, Map, Set)</li>
+        <li><strong>JDBC, File I/O and Serialization</strong></li>
+        <li><strong>Exception handling</strong> (checked and unchecked)</li>
+        <li>Working with the <strong>JVM and Memory management</strong></li>
+        <li>Basic understanding of <strong>Multithreading</strong> and synchronization</li>
+        <li><strong>Java GUI development</strong> with Swing/AWT and <strong>Java FX</strong>.</li>
         <li>Code modularization, and using Classes and Interfaces effectively</li>
-        <li>Knowledge of Guava: Google Core Libraries for Java</li>
+        <li>Working with external libraries and resources like <a href="https://github.com/google/guava">Guava</a> (Google Core Libraries for Java)</li>
       </ul>
     </div>
   </div>
