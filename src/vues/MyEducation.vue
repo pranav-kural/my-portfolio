3b28f4e8cb01ab26e68b3bcec8cb3cd82be93824
Pranav Kural <pranavkural042@gmail.com>
2017-05-13 15:12:25 -0400

    Added the My education section

---
+++ src/vues/MyEducation.vue
@@ -0,0 +1,44 @@
+<template lang="">
+  <div class="uk-container uk-flex uk-flex-center uk-padding">
+    <h1 class="uk-text-center uk-margin-remove" style="text-transform: uppercase;">Education</h1>
+    <hr class="divider">
+      <div class="uk-section uk-section-primary uk-preserve-color uk-padding-remove uk-container-small">
+          <div class="uk-padding">
+              <div class="uk-grid-match uk-child-width-1-1" uk-grid>
+                  <div>
+                      <div class="uk-card uk-card-default uk-card-body">
+                          <h4 class="uk-text-left uk-margin-remove">Computer Programming Diploma</h4>
+                          <p class="uk-margin-remove"><strong class="uk-text-left">Georgian College</strong><span class="rightFloat">January 2016 - December 2018 (Excepted)</span></p>
+                          <p class="uk-text-small">In this program, I learnt about computer programming, web development, and designing data-driven systems. I learnt how to write code in a variety of programming languages such as Arduino, ASP.NET, C#, Java, JavaScript, HTML/CSS, PHP and Swift. I also gained experience in developing software for diverse platforms including embedded systems, desktop, mobile and mainframe systems. With a strong emphasis on business and entrepreneurial values, I gained experience in problem solving, troubleshooting and system building through a series of applied assignments, projects and co-op work terms.</p>
+                      </div>
+                  </div>
+                  <div>
+                      <div class="uk-card uk-card-default uk-card-body">
+                        <h4 class="uk-text-left uk-margin-remove">High School</h4>
+                        <p class="uk-margin-remove"><strong class="uk-text-left">Om Parkash Bansal Modern School</strong><span class="rightFloat">1999 - 2015</span></p>
+                        <p class="uk-text-small">Final High School grade: 92.2%. Studied Economics, Accountancy, Business Management Studies and Multimedia and Application Development in high school.</p>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.rightFloat {
+  float: right;
+}
+hr.divider {
+    border-bottom: 2.5px solid #5a5a5a;
+    width: 60px;
+    margin: 10px 0 30px;
+    border-radius: 3px;
+}
+</style>
