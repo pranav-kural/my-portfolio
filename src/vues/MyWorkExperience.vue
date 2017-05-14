4c52cf707cde995e83e81d71e139b78cc2f8e6e4
Pranav Kural <pranavkural042@gmail.com>
2017-05-14 11:12:15 -0400

    Added the My Work Experience section

---
+++ src/vues/MyWorkExperience.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+  <div class="uk-container uk-flex uk-flex-center uk-padding">
+    <h1 class="uk-text-center uk-margin-remove" style="text-transform: uppercase;">My Work Experience</h1>
+    <hr class="divider-large">
+      <div class="uk-section uk-section-danger uk-preserve-color uk-padding-remove uk-container-small">
+          <div class="uk-padding">
+              <div class="uk-grid-match uk-child-width-1-1" uk-grid>
+                  <div>
+                      <div class="uk-card uk-card-default uk-card-body">
+                          <h4 class="uk-text-left uk-margin-remove">Web Developer</h4>
+                          <p class="uk-margin-remove"><strong class="uk-text-left">Center for Applied Research and Innovation, Georgian College</strong><span class="rightFloat">September 2016 - Present</span></p>
+                          <p class="uk-text-small">
+                            I worked as a fulltime web developer and Student Researcher in my first Co-op at the Center for Innovation and Applied Research (CARI), Georgian College. My primary tasks included:
+                            <br>
+                              <ul>
+                                  <li>Developing website based on the WordPress platform</li>
+                                  <li>Interacting with and managing clients</li>
+                                  <li>Designing websites from scratch (wireframing and prototyping)</li>
+                                  <li>Building, customizing and designing the themes based on client needs and user cases</li>
+                                  <li>Customizing plugins to support client needs</li>
+                                  <li>Training clients to use their website and the CMS</li>
+                                  <li>Representing CARI at certain events organized by CARI, Georgian College or partners. (Voluntarily).</li>
+                              </ul>
+                              Besides the technical skills (WordPress, Website designing, User Experience and website development), I also got the opportunity to learn and gain a lot soft skills:
+                              interacting with clients, assessing client needs, reporting the work or tasks completed in department (or team) meetings, formal as well as informal communication with
+                              the clients (or prospective clients), representing the CARI at events and in client meetings.
+                              <br>
+                              I was rehired as a part-time web developer in January 2017 after my co-op term ended in December 2016.
+                          </p>
+                      </div>
+                  </div>
+                  <div>
+                      <div class="uk-card uk-card-default uk-card-body">
+                        <h4 class="uk-text-left uk-margin-remove">Peer Tutor</h4>
+                        <p class="uk-margin-remove"><strong class="uk-text-left">Georgian College</strong><span class="rightFloat">September 2016 - April 2017</span></p>
+                        <p class="uk-text-small">
+                          Provides academic assistance to students experiencing difficulty in specific course content related to my field of study
+                          and expertise. This included providing tuitions to students, working with them to understand where they are having difficulties,
+                          motivating and encouraging them to learn more, clearing their doubts and showing them the best practices for programming and pointing them to helpful resources.
+                        </p>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.uk-section-danger {
+  background-color: #ec2147;
+}
+.uk-flex {
+  flex-direction: column;
+  align-items: center;
+}
+.uk-header-divider {
+  border-bottom: 2px solid #e5e5e5;
+}
+hr.divider-large {
+    border-bottom: 2.5px solid #5a5a5a;
+    width: 80px;
+    margin: 10px 0 30px;
+    border-radius: 3px;
+}
+</style>
