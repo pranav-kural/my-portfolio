6ffdc4365e0eac91f2568702ffc8f0bfef92d78a
Pranav Kural <pranavkural042@gmail.com>
2017-05-19 11:56:13 -0400

    Added the social media links

--- src/vues/Footer.vue
+++ src/vues/Footer.vue
@@ -1,5 +1,12 @@
 <template lang="html">
-
+<div id="footer">
+  <ul class="uk-list uk-flex uk-flex-middle uk- flex-center">
+                        <li><a href="https://ca.linkedin.com/in/pranav-kural-861623113"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                        <li><a href="https://twitter.com/pranav_kural"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+                        <li><a href="http://github.com/ifotn"><i class="fa fa-github-square" aria-hidden="true"></i></a></li>
+                        <li><a href="http://stackoverflow.com/users/5834653/pranav-kural"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a></li>
+                    </ul>
+</div>
 </template>
 
 <script>
@@ -9,4 +16,13 @@ export default {
 </script>
 
 <style lang="css">
+#footer ul {
+  flex-direction: row;
+  justify-content: center;
+  font-size: 2.2em;
+}
+#footer ul li {
+  margin: 15px;
+}
+
 </style>
