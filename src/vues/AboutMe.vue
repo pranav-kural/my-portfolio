ce5a5108d9b5df3df489c02c1786d4c2e64affc9
Pranav Kural <pranavkural042@gmail.com>
2017-05-19 10:02:02 -0400

    Updated the About vue design

--- src/vues/AboutMe.vue
+++ src/vues/AboutMe.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="container" uk-grid>
      <div id="aboutMeBackground" class="uk-width-2-5">
-       <div></div>
+       <div>
+         <div>
+         </div>
+       </div>
      </div>
      <div class="uk-width-3-5 uk-flex uk-flex-center uk-padding">
        <div id="aboutMeContainer" class="uk-padding">
@@ -33,11 +36,22 @@ div#aboutMeBackground {
 div#aboutMeBackground div {
   min-height: 100%;
   min-width: 100%;
-  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#ffffff+0,ffffff+82&0+36,1+100 */
-  background: -moz-linear-gradient(left,  rgba(255,255,255,0) 0%, rgba(255,255,255,0) 36%, rgba(255,255,255,0.72) 82%, rgba(255,255,255,1) 100%); /* FF3.6-15 */
-  background: -webkit-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(255,255,255,0) 36%,rgba(255,255,255,0.72) 82%,rgba(255,255,255,1) 100%); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(to right,  rgba(255,255,255,0) 0%,rgba(255,255,255,0) 36%,rgba(255,255,255,0.72) 82%,rgba(255,255,255,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#ffffff+0,ffffff+82&0+51,1+100 */
+  background: -moz-linear-gradient(left,  rgba(255,255,255,0) 0%, rgba(255,255,255,0) 51%, rgba(255,255,255,0.63) 82%, rgba(255,255,255,1) 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(255,255,255,0) 51%,rgba(255,255,255,0.63) 82%,rgba(255,255,255,1) 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to right,  rgba(255,255,255,0) 0%,rgba(255,255,255,0) 51%,rgba(255,255,255,0.63) 82%,rgba(255,255,255,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#ffffff',GradientType=1 ); /* IE6-9 */
+
+}
+
+div#aboutMeBackground div div {
+  min-height: 80vh;
+  min-width: 100%;
+  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#ffffff+0,ffffff+82&0+51,1+100 */
+  background: -moz-linear-gradient(top,  rgba(255,255,255,0) 0%, rgba(255,255,255,0) 51%, rgba(255,255,255,0.63) 82%, rgba(255,255,255,1) 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(top,  rgba(255,255,255,0) 0%,rgba(255,255,255,0) 51%,rgba(255,255,255,0.63) 82%,rgba(255,255,255,1) 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom,  rgba(255,255,255,0) 0%,rgba(255,255,255,0) 51%,rgba(255,255,255,0.63) 82%,rgba(255,255,255,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#ffffff',GradientType=0 ); /* IE6-9 */
 
 }
 
